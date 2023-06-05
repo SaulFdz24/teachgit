f13e53b335b3aafb21de312843b0071a396653f0
Fdiaz17 <70969449+Fdiaz17@users.noreply.github.com>
2023-06-05 15:39:40 -0600

    Update ActividadIntegradora1.py

--- ActividadIntegradora1.py
+++ ActividadIntegradora1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-import pandas_profiling
-from streamlit_pandas_profiling import st_profile_report
+#import pandas_profiling
+#from streamlit_pandas_profiling import st_profile_report
 #import numpy as np
 #import plotly as px
 #import plotly.figure_factory as ff
