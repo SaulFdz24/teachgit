1af0a7dab1cdf7ea9143d10fb35892c8189a4b71
Fdiaz17 <70969449+Fdiaz17@users.noreply.github.com>
2023-06-05 15:43:29 -0600

    Update ActividadIntegradora1.py

--- ActividadIntegradora1.py
+++ ActividadIntegradora1.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-#import pandas_profiling
-#from streamlit_pandas_profiling import st_profile_report
 #import numpy as np
 #import plotly as px
 #import plotly.figure_factory as ff
@@ -10,28 +8,27 @@ import pandas as pd
 
 st.title('Police Incident Reports from 2018 to 2020 in San Francisco')
 
-df = pd.read_csv("https://drive.google.com/drive/folders/1Hd2HOFknGccrm11VSdDp5N9-ADkboWvZ?usp=drive_link/Police_Department_Incident_Reports__2018_to_Present.csv")
+#df = pd.read_csv("https://drive.google.com/drive/folders/1Hd2HOFknGccrm11VSdDp5N9-ADkboWvZ?usp=drive_link/Police_Department_Incident_Reports__2018_to_Present.csv")
 
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
 
-mapa = pd.DataFrame()
-mapa['Date'] = df['Incident Date']
-mapa['Day'] = df['Incident Day of Week']
-mapa['Police District'] = df['Police District']
-mapa['Neighborhood'] = df['Analysis Neighborhood']
-mapa['Incident Category'] = df['Incident Category']
-mapa['Incident Subcategory'] = df['Incident Subcategory']
-mapa['Resolution'] = df['Resolution']
-mapa['lat'] = df['Latitude']
-mapa['lon'] = df['Longitude']
-mapa = mapa.dropna()
+#mapa = pd.DataFrame()
+#mapa['Date'] = df['Incident Date']
+#mapa['Day'] = df['Incident Day of Week']
+#mapa['Police District'] = df['Police District']
+#mapa['Neighborhood'] = df['Analysis Neighborhood']
+#mapa['Incident Category'] = df['Incident Category']
+#mapa['Incident Subcategory'] = df['Incident Subcategory']
+#mapa['Resolution'] = df['Resolution']
+#mapa['lat'] = df['Latitude']
+#mapa['lon'] = df['Longitude']
+#mapa = mapa.dropna()
 
-subset_data2 = mapa
-police_district_input = st.sidebar.multiselect(
-'Police District',
-mapa.groupby('Police District').count().reset_index()['Police District'].tolist())
-if len(police_district_input) > 0:
-    subset_data2 = mapa[mapa['Police District'].isin(police_district_input)]
+#subset_data2 = mapa
+#police_district_input = st.sidebar.multiselect('Police District',
+#mapa.groupby('Police District').count().reset_index()['Police District'].tolist())
+#if len(police_district_input) > 0:
+#    subset_data2 = mapa[mapa['Police District'].isin(police_district_input)]
 
 #subset_data1 = subset_data2
 #neighborhood_input = st.sidebar.multiselect('Neighborhood',
