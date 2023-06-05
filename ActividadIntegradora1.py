b9db82027449d047d4cbf90a0a36d311eec67e32
Fdiaz17 <70969449+Fdiaz17@users.noreply.github.com>
2023-06-05 15:46:41 -0600

    Update ActividadIntegradora1.py

--- ActividadIntegradora1.py
+++ ActividadIntegradora1.py
@@ -12,17 +12,17 @@ df = pd.read_csv("https://drive.google.com/drive/folders/1Hd2HOFknGccrm11VSdDp5N
 
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
 
-#mapa = pd.DataFrame()
-#mapa['Date'] = df['Incident Date']
-#mapa['Day'] = df['Incident Day of Week']
-#mapa['Police District'] = df['Police District']
-#mapa['Neighborhood'] = df['Analysis Neighborhood']
-#mapa['Incident Category'] = df['Incident Category']
-#mapa['Incident Subcategory'] = df['Incident Subcategory']
-#mapa['Resolution'] = df['Resolution']
-#mapa['lat'] = df['Latitude']
-#mapa['lon'] = df['Longitude']
-#mapa = mapa.dropna()
+mapa = pd.DataFrame()
+mapa['Date'] = df['Incident Date']
+mapa['Day'] = df['Incident Day of Week']
+mapa['Police District'] = df['Police District']
+mapa['Neighborhood'] = df['Analysis Neighborhood']
+mapa['Incident Category'] = df['Incident Category']
+mapa['Incident Subcategory'] = df['Incident Subcategory']
+mapa['Resolution'] = df['Resolution']
+mapa['lat'] = df['Latitude']
+mapa['lon'] = df['Longitude']
+mapa = mapa.dropna()
 
 #subset_data2 = mapa
 #police_district_input = st.sidebar.multiselect('Police District',
