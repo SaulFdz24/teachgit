6a15331b94993e2d73dba288d66726d77427be1a
Fdiaz17 <70969449+Fdiaz17@users.noreply.github.com>
2023-06-05 20:52:29 -0600

    Update ActividadIntegradora1.py

--- ActividadIntegradora1.py
+++ ActividadIntegradora1.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import pandas as pd
-import xarray as xr
 
-xarr = read_noaa("Police_Department_Incident_Reports__2018_to_Present")
+
+url = ' https://drive.google.com/drive/u/0/folders/1Hd2HOFknGccrm11VSdDp5N9-ADkboWvZ'
+path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
 st.title('Police Incident Reports from 2018 to 2020 in San Francisco')
 
-df = pd.read_csv("https://drive.google.com/file/d/1ioUp8979nNLh9h-CYVyJcRgAhxBhCLIm/view")
+df = pd.read_csv(path)
 st.dataframe(df)
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
 
