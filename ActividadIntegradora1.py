49aeb68a15faf84e7dad92d141da43035ab30460
Fdiaz17 <70969449+Fdiaz17@users.noreply.github.com>
2023-06-05 23:50:44 -0600

    Update ActividadIntegradora1.py

--- ActividadIntegradora1.py
+++ ActividadIntegradora1.py
@@ -9,8 +9,8 @@ for row in csv_file:
     print row
 st.title('Police Incident Reports from 2018 to 2020 in San Francisco')
 #df=get_translation_dict("Streamlit/Police.csv")
-df = pd.read_csv(".../content/Police_Department_Incident_Reports__2018_to_Present.csv")
-st.dataframe(df)
+#df = pd.read_csv(".../content/Police_Department_Incident_Reports__2018_to_Present.csv")
+#st.dataframe(df)
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
 
 mapa=pd.DataFrame()
