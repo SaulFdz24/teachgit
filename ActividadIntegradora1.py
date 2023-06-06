72afa30d800793dd011925976250ecc94cceccbf
Fdiaz17 <70969449+Fdiaz17@users.noreply.github.com>
2023-06-05 23:49:26 -0600

    Update ActividadIntegradora1.py

--- ActividadIntegradora1.py
+++ ActividadIntegradora1.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import pandas as pd
-
-
+import csv 
+import urllib2 
+ 
+response = urllib2.urlopen('https://drive.google.com/file/d/1WQ5QFkP9S1FfpAxCTxQpzWKEP5lD67uL/view?usp=drive_link) 
+csv_file = csv.reader(response) 
+for row in csv_file: 
+    print row
 st.title('Police Incident Reports from 2018 to 2020 in San Francisco')
 #df=get_translation_dict("Streamlit/Police.csv")
 df = pd.read_csv(".../content/Police_Department_Incident_Reports__2018_to_Present.csv")
