3e36769167e6e3b913529a7a433ff1df1fc625d5
Fdiaz17 <70969449+Fdiaz17@users.noreply.github.com>
2023-06-05 20:13:03 -0600

    Update ActividadIntegradora1.py

--- ActividadIntegradora1.py
+++ ActividadIntegradora1.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 
-st.title('https://drive.google.com/file/d/1ioUp8979nNLh9h-CYVyJcRgAhxBhCLIm/view')
+st.title('https://drive.google.com/file/d/1ioUp8979nNLh9h-CYVyJcRgAhxBhCLIm/view/Police_Department_Incident_Reports__2018_to_Present.csv')
 
 df = pd.read_csv("Police_Department_Incident_Reports__2018_to_Present.csv")
 st.dataframe(df)
