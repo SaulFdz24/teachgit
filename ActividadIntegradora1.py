42b81f6a3501c81a0dc452c6b6cf570a7daf0ec4
Fdiaz17 <70969449+Fdiaz17@users.noreply.github.com>
2023-06-05 21:00:10 -0600

    Update ActividadIntegradora1.py

--- ActividadIntegradora1.py
+++ ActividadIntegradora1.py
@@ -2,11 +2,9 @@ import streamlit as st
 import pandas as pd
 
 
-url = 'https://drive.google.com/file/d/1WQ5QFkP9S1FfpAxCTxQpzWKEP5lD67uL/view?usp=drive_link'
-path = 'https://drive.google.com/drive/u/0/folders/1Hd2HOFknGccrm11VSdDp5N9-ADkboWvZ'+url.split('/')[-2]
 st.title('Police Incident Reports from 2018 to 2020 in San Francisco')
 
-df = pd.read_csv(path)
+df = pd.read_csv("C:\Users\HP User\Police_Department_Incident_Reports__2018_to_Present")
 st.dataframe(df)
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
 
